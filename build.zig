8922d1ba9f8522012b9ce482a5482be78ffe2fe8
Johannes Teichrieb <isrvoid@gmail.com>
2023-02-01 12:09:52 +0100

    Adapt to new build options

--- build.zig
+++ build.zig
@@ -2,17 +2,23 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("webfb", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addExecutable(.{
+        .name = "webfb",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.install();
 
-    const wasm_lib = b.addSharedLibrary("webfb", "src/webfb_stub.zig", .unversioned);
     const wasm_target = std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-freestanding" }) catch unreachable;
-    wasm_lib.setTarget(wasm_target);
-    wasm_lib.setBuildMode(.ReleaseFast);
+    const wasm_lib = b.addSharedLibrary(.{
+        .name = "webfb",
+        .root_source_file = .{ .path = "src/webfb_stub.zig" },
+        .target = wasm_target,
+        .optimize = .ReleaseFast,
+    });
     wasm_lib.rdynamic = true;
     wasm_lib.strip = true;
     const wasm_page_size = 1 << 16;
@@ -25,14 +31,14 @@ pub fn build(b: *std.build.Builder) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
